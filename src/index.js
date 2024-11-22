73567fb63600b786fcf63b7daaf0af5ce1082435
Adrian Prado <adrian.prado@adadevelopersacademy.org>
2024-11-22 09:15:36 -0600

    Added index.html, styles.css and index.js starter code

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// Add javascript code to Party Parrot Clicker such that:
+
+// 1. A click on the Party Parrot increases the counter by 1
+// 2. Add a button called ‘reset’ that will reset the
+//    click count to ‘0’.
+// 3. Add an effect whenever the clickCount
+//    reaches a specific number.
+//    (Ex: make parrot bigger, smaller, change the text/emoji,
+//    etc.)
+
+// Extra:
+// // Box 1: When the mouse hovers over Box 1, switch the background to pink
+// Box 2: When the spacebar is pressed down, make the text in Box 2 turn into a flan emoji
+// Box 3: When Box 3 is clicked on, make Party Parrot appear and disappear
+// Box 4: When Box 4 is double clicked, an alert shows up
